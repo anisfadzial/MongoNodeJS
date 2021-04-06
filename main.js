34fde1d362b89ef5dad5f0c2c298524b728a7648
anisfadzial <anisfadzial99@gmail.com>
2021-04-06 22:09:58 +0800

    connect to mongo

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
 const {MongoClient} = require('mongodb');
+
 async function main(){
     /**
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.smxew.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://m001-mongodb-basics@sandbox.v4vjp.mongodb.net/myFirstDatabasetest?retryWrites=true&w=majority";
  
 
     const client = new MongoClient(uri);
  
     try {
         // Connect to the MongoDB cluster
-        await client.connect();
+        await client.connect();     //asynchronous process
  
-        // Make the appropriate DB calls
-        await  listDatabases(client);
+        const database = client.db("lab");
+        const collection = database.collection("members");
  
-    } catch (e) {
+        const res = await collection.insertMany(
+            [
+              {
+                name: "NURUL AAISYAH",
+                matric: "B021910026",
+                course:"BENR",
+                cohort:"2020/2021-1"
+              },
+              {
+                name: "NURUL ANIS BT FADZIAL",
+                matric: "B021910085",
+                course:"BENR",
+                cohort:"2020/2021-1"
+              },
+              {
+                name: "NUR SYAKIRAH BT TAJUDIN",
+                matric: "B021910088",
+                course:"BENR",
+                cohort:"2020/2021-1"
+              },
+              {
+                name: "SATES RAJ A/L JAYARAJ",
+                matric: "B021910028",
+                cohort:"2020/2021-1"
+              },
+
+            ]  
+        );
+
+        console.log(res)
+
+    } catch (e) {             //exception
         console.error(e);
     } finally {
         await client.close();
